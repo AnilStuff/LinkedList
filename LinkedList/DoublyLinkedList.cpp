97adfc671cca56ebbe6c1f3bc6ca5c8b880bd940
iRakesh <rakeshkothari1984@gmail.com>
2017-02-14 22:31:43 +0530

    Linked list file changes

---
+++ LinkedList/DoublyLinkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  DoublyLinkedList.cpp
+//  LinkedList
+//
+//  Created by Anil Kothari on 14/02/17.
+//  Copyright © 2017 Anil Kothari. All rights reserved.
+//
+
+#include <stdio.h>
--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -34,13 +34,80 @@ class List {
     
 public:
     List(){head = NULL;}
+    
+    //Appending the data in the linked list
     void appendData(int data);
+    
+    //Displaying the linked list
     void displayList();
+    
+    //Removing the last node of the linked list
     void popLastNode();
+    
+    //Removing the node of the linked list which contains the data
+    void popNodeWithData(int data);
+    
+    // -1 if not found in the location
+    int getNodeValueAtLocation(int Location);
+    
+    // TO find if data exists in the linked list
+    bool isNodeFoundWithData(int data);
+    
+    // To get the location of particular node data
+    int getLocationValueAtNodeValue(int value);
+    
+    // To insert a node after node data
+    void insertNodeAfterNodeData(int existingValue, int newValue);
+    
+    //To get middle element of the list
+    int getMiddleElement();
 };
 
 
+int List::getMiddleElement(){
+    
+    Node *n1 = head;
+    Node *n2 = head;
 
+    
+    while (n1 !=NULL) {
+        n1 = n1->getNextNode();
+        
+        if (n1!= NULL) {
+            n1 = n1->getNextNode();
+            n2 = n2->getNextNode();
+        }
+    }
+
+    return n2->getData();
+    
+}
+
+void List :: insertNodeAfterNodeData(int existingValue, int newValue){
+    Node *tempNode = new Node();
+    tempNode->setData(newValue);
+    tempNode->setNextNode(NULL);
+    
+    
+    Node *iteratorPtr = head;
+    
+    
+    while (iteratorPtr != NULL) {
+        
+        if (iteratorPtr->getData() == existingValue) {
+//            Node *node = iteratorPtr->getNextNode();
+            tempNode->setNextNode(iteratorPtr->getNextNode());
+            iteratorPtr->setNextNode(tempNode);
+            iteratorPtr = tempNode;
+
+        }else{
+            iteratorPtr = iteratorPtr->getNextNode();
+        }
+        
+    }
+    
+    
+}
 void List :: appendData(int data){
     
     Node *tempNode = new Node();
@@ -75,14 +142,110 @@ void List:: displayList(){
         return;
     }
         do {
+            cout << "Element in linked list at index " << iteratorPtr<< to_string(index)<< " is "<<iteratorPtr->getData()<<endl;
             index++;
-            cout << "Element in linked list at index " << to_string(index)<< " is "<<iteratorPtr->getData()<<endl;;
             iteratorPtr = iteratorPtr->getNextNode();
             
         }while (iteratorPtr != NULL);
         
     
  }
+
+bool List::isNodeFoundWithData(int data){
+    Node *iteratorPtr = head;
+    if (iteratorPtr == NULL) {
+        cout << "Empty List"<<endl;
+        return false;
+    }
+    
+    while (iteratorPtr != NULL) {
+        if (iteratorPtr ->getData() == data) {
+            return true;
+        }
+       iteratorPtr = iteratorPtr->getNextNode();
+    }
+    return false;
+}
+
+void List::popNodeWithData(int data){
+    Node *current = head;
+    Node *prev = NULL ;
+    
+    if (current == NULL ) {
+        cout << "Empty List"<<endl;
+        return;
+    }
+    if (current->getNextNode() == NULL) {
+        delete current;
+        head = NULL;
+        return;
+    }
+    else if (current->getData() == data){
+        prev = current;
+        head = head->getNextNode();
+    }
+    
+    else{
+    
+        do{
+            prev = current;
+
+            if (current->getData() == data) {
+                break;
+            }
+             current = current->getNextNode();
+            
+        }while (current->getData() != data);
+    
+     
+        
+    }
+    prev->setNextNode(current->getNextNode());
+    
+    delete current;
+    
+}
+
+
+int List:: getNodeValueAtLocation(int Location){
+    Node *iteratorPtr = head;
+    int Loc = 0;
+    
+    if (iteratorPtr == NULL) {
+        cout<<"List is empty"<<endl;
+        return -1;
+    }
+    
+    while (iteratorPtr != NULL) {
+        if (Loc == Location) {
+            return   iteratorPtr->getData();
+        }
+        iteratorPtr = iteratorPtr->getNextNode();
+        Loc++;
+    }
+    
+    return -1;
+ }
+
+int List:: getLocationValueAtNodeValue(int value){
+    Node *iteratorPtr = head;
+    int Loc = 0;
+    
+    while (iteratorPtr != NULL) {
+        if (value == iteratorPtr->getData()) {
+            return Loc  ;
+        }
+        iteratorPtr = iteratorPtr->getNextNode();
+        Loc++;
+    }
+    
+    return -1;
+}
+
+
+
+
+
 
 void List:: popLastNode(){
     Node *iteratorPtr = head;
@@ -106,7 +269,7 @@ void List:: popLastNode(){
         iteratorPtr = iteratorPtr->getNextNode();
     }
     
-    iteratorPtr->setNextNode(NULL);//() = NULL;
+      iteratorPtr->setNextNode(NULL);//() = NULL;
     
 }
 
@@ -114,19 +277,32 @@ void List:: popLastNode(){
 
 int main(int argc, const char * argv[]) {
     
-     int ch;
+    int ch;
     int number;
-    
+    int existingValue,newValue;
+
     List list = *new List();
     
-    
-    while (ch != 'e') {
-        cout<< "-------------------------------------"<<endl;
+    list.appendData(13);
+    list.appendData(532);
+    list.appendData(23);
+    list.appendData(52);
+    list.appendData(522);
+    list.appendData(522);
+    list.displayList();
+
+    while (ch != 9) {
+        cout<< "------------------------------------------------"<<endl;
         cout<< "Enter 1 to append data in list"<<endl;
-        cout<< "Enter 2 to end the program"<<endl;
+        cout<< "Enter 2 to get value at particular location"<<endl;
         cout<< "Enter 3 to display data in list"<<endl;
         cout<< "Enter 4 to pop last element"<<endl;
-        cout<< "-- -----------------------------------"<<endl;
+        cout<< "Enter 5 to pop specific element"<<endl;
+        cout<< "Enter 6 to search specific element in list"<<endl;
+        cout<< "Enter 7 to insert specific element at location in list"<<endl;
+        cout<< "Enter 8 to Find middle element of list"<<endl;
+        cout<< "Enter 9 to exit"<<endl;
+        cout<< "-- ----------------------------------------------"<<endl;
 
         cin >> ch;
         
@@ -141,12 +317,42 @@ int main(int argc, const char * argv[]) {
                 list.displayList();
                 break;
             case 2:
-                exit(0);
+                cout<<"Enter Location which needs to be found : ";
+                cin>>number;
+                cout << "Node value at location "<<number<<"is"<<list.getNodeValueAtLocation(number)<<endl;
                 break;
             case 4:
                 list.popLastNode();
                 break;
-            default:
+            case 5:
+                cout<< "Enter data(Integer) : ";
+                cin >> number;
+                list.popNodeWithData(number);
+                break;
+            case 6:
+                cout<< "Enter data(Integer) : ";
+                cin >> number;
+                
+                if (list.isNodeFoundWithData(number)) {
+                    cout<<"Number Found in list at location "<<list.getLocationValueAtNodeValue(number)<<endl;
+                }else{
+                    cout<<"Data not present in list";
+                }
+            break;
+                
+            case 7:
+                cout<<"Enter existing value, new value";
+                cin>>existingValue;
+                cin>>newValue;
+                list.insertNodeAfterNodeData(existingValue, newValue);
+                break;
+                
+            case 8:{
+                cout<<"Middle Element is  "<<list.getMiddleElement()<<endl;
+            }
+            break;
+            case 9:
+                exit(0);
                 break;
         }
     }
